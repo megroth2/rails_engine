2e3edc8e8a0da89dde2156cab0252d3ed749a714
Nash <141100397+s2an@users.noreply.github.com>
2024-02-07 15:59:53 -0500

    Fetch a single item and pass RSpec and Postman

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -3,4 +3,8 @@ class Api::V1::ItemsController < ApplicationController
     items = Item.all
     render json: ItemSerializer.new(items)
   end
+
+  def show
+    render json: ItemSerializer.new(Item.find(params[:id]))
+  end
 end
