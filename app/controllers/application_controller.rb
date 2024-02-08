67dea62da2b0c9b7b3c26561242577ed552e2776
Meg Roth <146018731+megroth2@users.noreply.github.com>
2024-02-08 13:12:05 -0700

    debug final postman test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::InvalidForeignKey, with: :not_found_response
-  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
 
 
   private
@@ -9,9 +9,5 @@ class ApplicationController < ActionController::API
     render json: ErrorSerializer.new(
       ErrorMessage.new(exception.message, 404)
       ).serialize_json, status: :not_found
-  end
-
-  def record_not_found
-    render json: { error: "Record not found" }, status: :not_found
   end
 end
