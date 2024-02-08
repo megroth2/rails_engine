56b06f84f1c10b211267489b8bea871709da9cfa
Meg Roth <146018731+megroth2@users.noreply.github.com>
2024-02-07 20:33:32 -0700

    wip: update an item

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -3,4 +3,24 @@ class Api::V1::ItemsController < ApplicationController
     items = Item.all
     render json: ItemSerializer.new(items)
   end
+
+  def show
+  end
+
+  def update
+    item = Item.find(params[:id]) # whats the best way to implement error handling for when the item is not found? Would it be to add a helper method with if else statements like the one below?
+    if item.update!(item_params) 
+      render json: ItemSerializer.new(item)
+      # render json: Item.update(params[:id], item_params)
+    else
+      # Add Error Handling
+    end
+  end
+
+  private
+
+    def item_params
+      params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+      # binding.pry
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants, :index
-      resources :items, :index
+      resources :items, :index, :update
     end
   end
 
