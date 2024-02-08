b338161bf6f3689a285970db7264136b6ae77ea2
Meg Roth <146018731+megroth2@users.noreply.github.com>
2024-02-08 02:47:24 -0700

    wip: signing off for the night

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,71 +1,119 @@
 require "rails_helper"
 
 describe "Merchants API" do
-  it "sends a list of merchants" do
-    FactoryBot.create_list(:merchant, 100)
+  describe "get all merchants" do
+    it "sends a list of merchants" do
+      FactoryBot.create_list(:merchant, 10)
 
-    get "/api/v1/merchants"
+      get "/api/v1/merchants"
 
-    expect(response).to be_successful
-    merchants = JSON.parse(response.body, symbolize_names: :true)
+      expect(response).to be_successful
+      merchants = JSON.parse(response.body, symbolize_names: :true)
 
-    expect(merchants[:data].count).to eq(100)
+      expect(merchants[:data].count).to eq(10)
 
-    merchants[:data].each do |merchant|
-      expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to be_a(String)
+      merchants[:data].each do |merchant|
+        expect(merchant).to have_key(:id)
+        expect(merchant[:id]).to be_a(String)
+        
+        expect(merchant[:attributes]).to have_key(:name)
+        expect(merchant[:attributes][:name]).to be_a(String)
+      end
+    end
+
+    it "returns an array even if zero merchants are found" do
+      get "/api/v1/merchants"
+
+      merchants = JSON.parse(response.body, symbolize_names: :true)
+
+      expect(merchants[:data].count).to eq(0)
+      expect(merchants[:data]).to eq([])
+    end
+
+    it "returns an array even if one merchant is found" do
+      FactoryBot.create(:merchant)
+
+      get "/api/v1/merchants"
+
+      merchants = JSON.parse(response.body, symbolize_names: :true)
+
+      expect(merchants[:data].count).to eq(1)
+      expect(merchants[:data]).to be_an(Array)
+    end
+
+    it "does not include dependent data of the resource" do
+      FactoryBot.create_list(:merchant, 10)
+
+      get "/api/v1/merchants"
+
+      merchants = JSON.parse(response.body, symbolize_names: :true)
+
+      merchants[:data].each do |merchant|
+        expect(merchant).to_not have_key(:invoice)
+        expect(merchant).to_not have_key(:item)
+      end
+    end
+  end
+
+  describe "get one merchant" do
+    it "sends one merchant" do
+      merchant = FactoryBot.create(:merchant)
+
+      get "/api/v1/merchants/#{merchant.id}"
+
+      expect(response).to be_successful
+
+      merchant = JSON.parse(response.body, symbolize_names: :true)
+
+      expect(merchant[:data]).to have_key(:id)
+      expect(merchant[:data][:id]).to be_a(String)
+
+      expect(merchant[:data][:attributes]).to have_key(:name)
+      expect(merchant[:data][:attributes][:name]).to be_a(String)
+    end
+  end
+
+  describe "get a merchant's items" do
+    it "sends a list of items for a given merchant" do
+      merchant = FactoryBot.create(:merchant)
+      merchant_items = FactoryBot.create_list(:item, 10, merchant_id: merchant.id)
+
+      get "/api/v1/merchants/#{merchant.id}/items"
+
+      expect(response).to be_successful
+      merchant_items = JSON.parse(response.body, symbolize_names: :true)
+
+      expect(merchant_items[:data].count).to eq(10)
+    end
+
+    it "does NOT send items from another merchant" do
+      merchant_1 = FactoryBot.create(:merchant)
+      merchant_2 = FactoryBot.create(:merchant)
+
+      merchant_1_items = FactoryBot.create_list(:item, 6, merchant_id: merchant_1.id)
+      merchant_2_items = FactoryBot.create_list(:item, 3, merchant_id: merchant_2.id)
+
+      get "/api/v1/merchants/#{merchant_1.id}/items"
       
-      expect(merchant[:attributes]).to have_key(:name)
-      expect(merchant[:attributes][:name]).to be_a(String)
+      expect(response).to be_successful
+      merchant_items = JSON.parse(response.body, symbolize_names: :true)
+      
+      # binding.pry
+      expect(merchant_items[:data].count).to eq(6)
+
+      get "/api/v1/merchants/#{merchant_2.id}/items"
+
+      expect(response).to be_successful # why is the response body returning all items and not the merchant's items?
+      merchant_items = JSON.parse(response.body, symbolize_names: :true)
+
+      expect(merchant_items[:data].count).to eq(3)
     end
-  end
 
-  it "returns an array even if zero merchants are found" do
-    get "/api/v1/merchants"
+    it "returns a 404 HTTP status code if merchant is not found" do
+      get "/api/v1/merchants/1/items"
 
-    merchants = JSON.parse(response.body, symbolize_names: :true)
-
-    expect(merchants[:data].count).to eq(0)
-    expect(merchants[:data]).to eq([])
-  end
-
-  it "returns an array even if one merchant is found" do
-    FactoryBot.create(:merchant)
-
-    get "/api/v1/merchants"
-
-    merchants = JSON.parse(response.body, symbolize_names: :true)
-
-    expect(merchants[:data].count).to eq(1)
-    expect(merchants[:data]).to be_an(Array)
-  end
-
-  it "does not include dependent data of the resource" do
-    FactoryBot.create_list(:merchant, 10)
-
-    get "/api/v1/merchants"
-
-    merchants = JSON.parse(response.body, symbolize_names: :true)
-
-    merchants[:data].each do |merchant|
-      expect(merchant).to_not have_key(:invoice)
-      expect(merchant).to_not have_key(:item)
+      expect(response).to be_successful
+      expect(response.status).to eq (404)
     end
-  end
-
-  it "sends one merchant" do
-    merchant = FactoryBot.create(:merchant)
-
-    get "/api/v1/merchants/#{merchant.id}"
-
-    expect(response).to be_successful
-
-    merchant = JSON.parse(response.body, symbolize_names: :true)
-
-    expect(merchant[:data]).to have_key(:id)
-    expect(merchant[:data][:id]).to be_a(String)
-
-    expect(merchant[:data][:attributes]).to have_key(:name)
-    expect(merchant[:data][:attributes][:name]).to be_a(String)
   end
 end
