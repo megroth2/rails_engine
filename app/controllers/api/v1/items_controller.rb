17614ce18c25978e4e30e272a1be676e85011f7b
Meg Roth <146018731+megroth2@users.noreply.github.com>
2024-02-07 21:17:03 -0700

    wip: delete an item

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -7,4 +7,11 @@ class Api::V1::ItemsController < ApplicationController
   def show
     render json: ItemSerializer.new(Item.find(params[:id]))
   end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.invoice_items.destroy # can't destroy an item without first destroying invoice items
+    item.destroy
+    render json: ItemSerializer.new(item)
+  end
 end
--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -1,86 +1,130 @@
 require "rails_helper"
 
 describe "Items API" do
-  it "sends a list of items" do
-    merchant = FactoryBot.create(:merchant)
-    FactoryBot.create_list(:item, 10, merchant_id: merchant.id)
+  describe "get all items" do
+    it "sends a list of items" do
+      merchant = FactoryBot.create(:merchant)
+      FactoryBot.create_list(:item, 10, merchant_id: merchant.id)
 
-    get "/api/v1/items"
+      get "/api/v1/items"
 
-    expect(response).to be_successful
-    items = JSON.parse(response.body, symbolize_names: :true)
+      expect(response).to be_successful
+      items = JSON.parse(response.body, symbolize_names: :true)
 
-    expect(items[:data].count).to eq(10)
+      expect(items[:data].count).to eq(10)
 
-    items[:data].each do |item|
-      expect(item).to have_key(:id)
-      expect(item[:id]).to be_a(String)
+      items[:data].each do |item|
+        expect(item).to have_key(:id)
+        expect(item[:id]).to be_a(String)
 
-      expect(item[:attributes]).to have_key(:name)
-      expect(item[:attributes][:name]).to be_a(String)
+        expect(item[:attributes]).to have_key(:name)
+        expect(item[:attributes][:name]).to be_a(String)
 
-      expect(item[:attributes]).to have_key(:description)
-      expect(item[:attributes][:description]).to be_a(String)
+        expect(item[:attributes]).to have_key(:description)
+        expect(item[:attributes][:description]).to be_a(String)
 
-      expect(item[:attributes]).to have_key(:unit_price)
-      expect(item[:attributes][:unit_price]).to be_a(Float)
+        expect(item[:attributes]).to have_key(:unit_price)
+        expect(item[:attributes][:unit_price]).to be_a(Float)
 
-      expect(item[:attributes]).to have_key(:merchant_id)
-      expect(item[:attributes][:merchant_id]).to be_an(Integer)
+        expect(item[:attributes]).to have_key(:merchant_id)
+        expect(item[:attributes][:merchant_id]).to be_an(Integer)
+      end
+    end
+
+    it "returns an array even if zero items are found" do
+      get "/api/v1/items"
+
+      items = JSON.parse(response.body, symbolize_names: :true)
+
+      expect(items[:data].count).to eq(0)
+      expect(items[:data]).to eq([])
+    end
+
+    it "returns an array even if one item is found" do
+      merchant = FactoryBot.create(:merchant)
+      FactoryBot.create(:item, merchant_id: merchant.id)
+
+      get "/api/v1/items"
+
+      items = JSON.parse(response.body, symbolize_names: :true)
+
+      expect(items[:data].count).to eq(1)
+      expect(items[:data]).to be_an(Array)
+    end
+
+    it "does not include dependent data of the resource" do
+      merchant = FactoryBot.create(:merchant)
+      FactoryBot.create_list(:item, 10, merchant_id: merchant.id)
+
+      get "/api/v1/items"
+
+      items = JSON.parse(response.body, symbolize_names: :true)
+
+      items[:data].each do |item|
+        expect(items).to_not have_key(:merchant_name)
+        expect(items).to_not have_key(:item)
+      end
     end
   end
 
-  it "returns an array even if zero items are found" do
-    get "/api/v1/items"
+  describe "get one item" do
+    it "sends one item" do
+      merchant = FactoryBot.create(:merchant)
+      item = FactoryBot.create(:item, merchant_id: merchant.id)
 
-    items = JSON.parse(response.body, symbolize_names: :true)
+      get "/api/v1/items/#{item.id}"
 
-    expect(items[:data].count).to eq(0)
-    expect(items[:data]).to eq([])
-  end
+      expect(response).to be_successful
 
-  it "returns an array even if one item is found" do
-    merchant = FactoryBot.create(:merchant)
-    FactoryBot.create(:item, merchant_id: merchant.id)
+      item = JSON.parse(response.body, symbolize_names: :true)
 
-    get "/api/v1/items"
+      expect(item[:data]).to have_key(:id)
+      expect(item[:data][:id]).to be_a(String)
 
-    items = JSON.parse(response.body, symbolize_names: :true)
-
-    expect(items[:data].count).to eq(1)
-    expect(items[:data]).to be_an(Array)
-  end
-
-  it "does not include dependent data of the resource" do
-    merchant = FactoryBot.create(:merchant)
-    FactoryBot.create_list(:item, 10, merchant_id: merchant.id)
-
-    get "/api/v1/items"
-
-    items = JSON.parse(response.body, symbolize_names: :true)
-
-    items[:data].each do |item|
-      expect(items).to_not have_key(:merchant_name)
-      expect(items).to_not have_key(:item)
+      expect(item[:data][:attributes]).to have_key(:name)
+      expect(item[:data][:attributes][:name]).to be_a(String)
+      expect(item[:data][:attributes][:description]).to be_a(String)
+      expect(item[:data][:attributes][:unit_price]).to be_a(Float)
     end
   end
 
-  it "sends one item" do
-    merchant = FactoryBot.create(:merchant)
-    item = FactoryBot.create(:item, merchant_id: merchant.id)
+  describe "delete an item" do
+    it "can delete an item" do # destroys the corresponsing record and any associated data
+      merchant = FactoryBot.create(:merchant)
+      item = FactoryBot.create(:item, merchant_id: merchant.id)
+  
+      expect(Item.count).to eq(1)
+    
+      delete "/api/v1/items/#{item.id}"
+    
+      expect(response).to be_successful
+      expect(Item.count).to eq(0)
+      expect{Item.find(item.id)}.to raise_error(ActiveRecord::RecordNotFound)
+    end
 
-    get "/api/v1/items/#{item.id}"
+    it "destroys any invoice where this was the only item on an invoice" do
+      merchant = FactoryBot.create(:merchant)
+      item = FactoryBot.create(:item, merchant_id: merchant.id)
 
-    expect(response).to be_successful
+      customer = FactoryBot.create(:customer)
+      invoice = FactoryBot.create(:invoice, merchant_id: merchant.id, customer_id: customer.id)
+      invoice_item = FactoryBot.create(:invoice_item, item_id: item.id, invoice_id: invoice.id)
 
-    item = JSON.parse(response.body, symbolize_names: :true)
+      expect(Invoice.count).to eq(1)
 
-    expect(item[:data]).to have_key(:id)
-    expect(item[:data][:id]).to be_a(String)
+      delete "/api/v1/items/#{item.id}"
 
-    expect(item[:data][:attributes]).to have_key(:name)
-    expect(item[:data][:attributes][:name]).to be_a(String)
-    expect(item[:data][:attributes][:description]).to be_a(String)
-    expect(item[:data][:attributes][:unit_price]).to be_a(Float)
+      expect(response).to be_successful
+      expect(Invoice.count).to eq(0)
+      expect{Invoice.find(invoice.id)}.to raise_error(ActiveRecord::RecordNotFound)
+    end
+
+    xit "returns a 204 HTTP status code" do
+
+    end
+
+    xit "doesn't return any JSON body" do
+
+    end
   end
 end
