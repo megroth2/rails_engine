e2d1d629af6248692db8d2baa9e46939c72522c1
Nash <141100397+s2an@users.noreply.github.com>
2024-02-09 00:11:44 -0500

    Pass postman tests for User Story 7 and 8

--- app/controllers/api/v1/find_items_controller.rb
+++ app/controllers/api/v1/find_items_controller.rb
@@ -6,12 +6,20 @@ class Api::V1::FindItemsController < ApplicationController
       render json: { error: "Cannot search by both name and price" }, status: :bad_request
     elsif params[:name]
       render json: ItemSerializer.new(Item
-      .where("lower(name) LIKE ?", "%#{params[:name].downcase}%")
-      .order(:name))
-    elsif params[:min_price] || params[:max_price]
+        .where("lower(name) LIKE ?", "%#{params[:name].downcase}%")
+        .order(:name))
+    elsif params[:min_price] && params[:min_price].to_f < 0
+      render json: { errors: "Min price less than 0" }, status: :bad_request
+    elsif params[:max_price] && params[:max_price].to_f < 0
+      render json: { errors: "Max price less than 0" }, status: :bad_request  
+    elsif params[:min_price] 
       render json: ItemSerializer.new(Item
-      .where(:unit_price >= params[:min_price] && :unit_price <= params[:max_price])
-      .order(:name))
+        .where('unit_price >= ?', params[:min_price])
+        .order(:name))
+    elsif params[:max_price]
+      render json: ItemSerializer.new(Item
+      .where('unit_price <= ?', params[:max_price])
+        .order(:name))
     else
       render json: { error: "No param given" }, status: :bad_request
     end
